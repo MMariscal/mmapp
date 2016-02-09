aeb61d613dfb1009b1d90d4a807e90ceaabfc71b
Marcos Mariscal <marcos.mariscal@icloud.com>
2016-02-09 15:49:14 +0100

    navbar_app.php
    Hacer que aparezca el usuario conectado, tal y como está en el navbar
    original.

--- php/navbar_app.php
+++ php/navbar_app.php
@@ -8,24 +8,23 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="app/index.php"><b>OcreAPP</b></a>
+            <a class="navbar-brand" href="index.php"><b>OcreAPP</b></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
             <ul class="nav navbar-nav">
-                <?php if(!isset($_SESSION["user_id"])):?>
-                    <li><a href="./registro.php">REGISTRO</a></li>
-                    <li><a href="./login.php">LOGIN</a></li>
-                <?php else:?>
-                    <li><a href="./php/logout.php">SALIR</a></li>
-                <?php endif;?>
-            </ul>
-            <ul class="nav navbar-nav">
-                <li>
-                    <?php                    
-                    ?>
-                </li>
+                <?php 
+                    if (!isset($_SESSION["user_id"])){
+                        echo '<li><a href="../registro.php">REGISTRO</a></li>';
+                        echo '<li><a href="../login.php">LOGIN</a></li>';
+                    }else{
+                        echo '<li><a href="../php/logout.php">SALIR</a></li>';
+                    }
+                    if (isset($_SESSION["fullname"])){
+                        echo '<li><a href="#">Ha iniciado sesión como: '.$_SESSION["fullname"].'</a></li>';
+                    }
+                ?>
             </ul>
         </div><!-- /.navbar-collapse -->
     </div>
